bc33bf095809743cafd7af0b23286ba601dfd00e
avijitprasad <avijits01@gmail.com>
2019-04-03 21:53:22 +0530

    Added a new activity. Edit Notes. Run the first version of notes offline app.

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -12,10 +14,14 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
      private Toolbar mainToolbar;
      private FirebaseAuth mAuth;
+     ArrayList<String> notes =new ArrayList<>(); //creates an array list to implement adapter in the list view
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +30,12 @@ public class MainActivity extends AppCompatActivity {
         //mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         getSupportActionBar().setTitle("Icas App"); //this putts the title to the main page
         mAuth = FirebaseAuth.getInstance(); //gets the current object that handles the user
-        
-
+        ListView listView= (ListView) findViewById(R.id.list_view);
+        String s="Avijit";
+        String j="My work";
+        notes.add(j+"\n -"+s);
+        ArrayAdapter<String> arrayAdapter =new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(arrayAdapter);
     }
 
     @Override
@@ -34,12 +44,12 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         FirebaseApp.initializeApp(this);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();//checks if a user is logged in
-        if (user == null) {
+        if (user == null) { //if there is no user. Login Activity starts
             sendTologIn();
         }
         else
         {
-            if(user.getDisplayName()==null)
+            if(user.getDisplayName()==null) //if the user is not registered registeration is done first
             {
                 Intent intent=new Intent(MainActivity.this,SetupActivity.class);
                 startActivity(intent);
